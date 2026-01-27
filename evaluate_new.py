11302ffa41ec5c2f07f6e00d7434dd1f0f154e10
Johannes Kiechle <johannes.kiechle@tum.de>
2026-01-27 18:37:19 +0100

    Refactor data loading in evaluate_new.py to simplify DataLoader initialization and remove unused sample weighting logic

--- evaluate_new.py
+++ evaluate_new.py
@@ -23,7 +23,7 @@ from utils.graph_utils import make_directed_complete_forward_graph
 # from models.Janickova import ResNet18Encoder
 from data.Dataset import ISPY2
 from sklearn.decomposition import PCA
-from imblearn.pipeline import Pipeline
+# from imblearn.pipeline import Pipeline
 from numpy import mean
 from numpy import std
 from sklearn.model_selection import StratifiedKFold, KFold
@@ -34,7 +34,7 @@ from glob import glob
 import torch
 import numpy as np
 from sklearn.decomposition import PCA
-from imblearn.pipeline import Pipeline
+# from imblearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 # from imblearn.over_sampling import SMOTE
 from sklearn.svm import SVC
@@ -98,51 +98,16 @@ def main(method, timepoints, fold, checkpoint_path):
     # datasets
     train_dataset = ISPY2(split='train', fold=fold, timepoints=timepoints)
     val_dataset = ISPY2(split='val', fold=fold, timepoints=timepoints)
-    test_dataset = ISPY2(split='test', fold=fold, timepoints=timepoints)
+    test_dataset = ISPY2(split='test', fold=fold, timepoints=timepoints)    
 
-    # Count samples per class
-    patient_ids = torch.load(f"./data/breast_cancer/data_splits_{timepoints}_timepoints.pt")[fold]["train"]
-    
-    labels = []
-    for patient_id in patient_ids:
-        files = sorted(glob(f"./data/breast_cancer/data_processed/{patient_id}/*.pt"))
-        label = files[0].split('/')[-1].split('_')[-1].replace('.pt', '')
-        labels.append(int(label)) 
-    labels = np.array(labels)
-    
-    class_counts = np.bincount(labels)
-
-    # Inverse frequency as weights
-    class_weights = 1.0 / class_counts
-
-    # Assign weight to each sample
-    sample_weights = class_weights[labels]
-
-    sample_weights = torch.DoubleTensor(sample_weights)
-
-    # Sampler
-    sampler = torch.utils.data.WeightedRandomSampler(
-        weights=sample_weights,
-        num_samples=len(sample_weights),
-        replacement=True
-    )
-
-    g = torch.Generator()
-    g.manual_seed(42)
-
-    train_dl = torch.utils.data.DataLoader(train_dataset, batch_size=16, shuffle=False, num_workers=4, sampler=sampler, persistent_workers=True, worker_init_fn=seed_worker, generator=g)
-    val_dl = torch.utils.data.DataLoader(val_dataset, batch_size=16, shuffle=False, num_workers=4, persistent_workers=True, worker_init_fn=seed_worker, generator=g)
-    test_dl = torch.utils.data.DataLoader(test_dataset, batch_size=16, shuffle=False, num_workers=4, persistent_workers=True, worker_init_fn=seed_worker, generator=g)
+    train_dl = torch.utils.data.DataLoader(train_dataset, batch_size=16, shuffle=False, num_workers=4)
+    val_dl = torch.utils.data.DataLoader(val_dataset, batch_size=16, shuffle=False, num_workers=4)
+    test_dl = torch.utils.data.DataLoader(test_dataset, batch_size=16, shuffle=False, num_workers=4)
 
     if method == "kaczmarek":
         model = ResNet18EncoderKaczmarek(timepoints=timepoints).to(device)
         model_num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
         mlflow.log_param('model_num_params', model_num_params)
-
-        perms = list(itertools.permutations(range(timepoints), timepoints))
-        perms = [''.join([str(item) for item in p]) for p in perms]
-        enc = OneHotEncoder()
-        enc.fit(np.array(perms).reshape(-1, 1))
 
     elif method == "janickova":
         model = ResNet18EncoderJanickova().to(device)
@@ -279,32 +244,38 @@ def main(method, timepoints, fold, checkpoint_path):
     print(f"AUROC             : {auroc:.3f}")
     print(f"Sensitivity       : {sensitivity:.3f}")
     print(f"Specificity       : {specificity:.3f}")
+
+    mlflow.log_metric('test_balanced_accuracy', bal_acc)
+    mlflow.log_metric('test_mcc', mcc)
+    mlflow.log_metric('test_auroc', auroc)
+    mlflow.log_metric('test_sensitivity', sensitivity)
+    mlflow.log_metric('test_specificity', specificity)
         
 
 if __name__ == '__main__':  
 
     janickova_dict = {
-        "fold_0_best_metric": "",
-        "fold_1_best_metric": "",
-        "fold_2_best_metric": "",
-        "fold_3_best_metric": "",
-        "fold_4_best_metric": "",
+        "fold_0_best_metric": "/home/johannes/Code/GNN_pCR/mlruns/3/6b5e7d28c10f45bb9764249674071194/artifacts/model_best_metric.pt",
+        "fold_1_best_metric": "/home/johannes/Code/GNN_pCR/mlruns/3/f8d20bc2b7f24ef59d32cd39bc450822/artifacts/model_best_metric.pt",
+        "fold_2_best_metric": "/home/johannes/Code/GNN_pCR/mlruns/3/c9e29e39047a4ecb875e9aefa11dde41/artifacts/model_best_metric.pt",
+        "fold_3_best_metric": "/home/johannes/Code/GNN_pCR/mlruns/3/8ca90dc5a545447ab92ad3558021bf9a/artifacts/model_best_metric.pt",
+        "fold_4_best_metric": "/home/johannes/Code/GNN_pCR/mlruns/3/d3122546d2584227bbebb1ac69099abe/artifacts/model_best_metric.pt",
     }
 
     kaczmarek_dict = {
-        "fold_0_best_metric": "",
-        "fold_1_best_metric": "",
-        "fold_2_best_metric": "",
-        "fold_3_best_metric": "",
-        "fold_4_best_metric": "",
+        "fold_0_best_metric": "/home/johannes/Code/GNN_pCR/mlruns/3/0a21a08722934f0c9828562f28c40228/artifacts/model_best_metric.pt",
+        "fold_1_best_metric": "/home/johannes/Code/GNN_pCR/mlruns/3/365c1881d08842c991f946842dd50099/artifacts/model_best_metric.pt",
+        "fold_2_best_metric": "/home/johannes/Code/GNN_pCR/mlruns/3/9001a9be12e748109ca1733bf94b25ff/artifacts/model_best_metric.pt",
+        "fold_3_best_metric": "/home/johannes/Code/GNN_pCR/mlruns/3/db5f8ab18a6c43f3ac0683ae7b0afe08/artifacts/model_best_metric.pt",
+        "fold_4_best_metric": "/home/johannes/Code/GNN_pCR/mlruns/3/15990b1778f048e6af52e03a94d2632c/artifacts/model_best_metric.pt",
     }
     
     kiechle_dict = {
-        "fold_0_best_metric": "",
-        "fold_1_best_metric": "",
-        "fold_2_best_metric": "",
-        "fold_3_best_metric": "",
-        "fold_4_best_metric": "",
+        "fold_0_best_metric": "/home/johannes/Code/GNN_pCR/mlruns/3/a00da610e4ec4ba786ef63abbdac5ab7/artifacts/model_best_metric.pt",
+        "fold_1_best_metric": "/home/johannes/Code/GNN_pCR/mlruns/3/c724a86d33284fc298283f32ff207a61/artifacts/model_best_metric.pt",
+        "fold_2_best_metric": "/home/johannes/Code/GNN_pCR/mlruns/3/c724a86d33284fc298283f32ff207a61/artifacts/model_best_metric.pt",
+        "fold_3_best_metric": "/home/johannes/Code/GNN_pCR/mlruns/3/ab7a282b007a44dcb8abfd041b2db7ac/artifacts/model_best_metric.pt",
+        "fold_4_best_metric": "/home/johannes/Code/GNN_pCR/mlruns/3/90b66fc6c2d94dbea2e90681b54b218e/artifacts/model_best_metric.pt",
     }
 
     checkpoints_dict = {
