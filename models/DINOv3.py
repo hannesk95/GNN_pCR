455fd717fb245fba63f9a25d159cdce5426814de
Johannes Kiechle <johannes.kiechle@tum.de>
2026-02-12 14:17:35 +0100

    Refactor DINOv3 constructor to improve model name handling; clean up commented options

--- models/DINOv3.py
+++ models/DINOv3.py
@@ -2,7 +2,12 @@ import torch
 from transformers import AutoImageProcessor, AutoModel
 
 class DINOv3(torch.nn.Module):
-    def __init__(self, model_name='facebook/dinov3-vits16-pretrain-lvd1689m'):
+    def __init__(self, 
+                #  model_name='facebook/dinov3-vits16-pretrain-lvd1689m',
+                #  model_name='facebook/dinov3-vitb16-pretrain-lvd1689m',
+                 model_name='facebook/dinov3-vitl16-pretrain-lvd1689m',
+                #  model_name='facebook/dinov3-vit7b16-pretrain-lvd1689m',
+                 ):
         super(DINOv3, self).__init__()
         self.image_processor = AutoImageProcessor.from_pretrained(model_name)
         self.model = AutoModel.from_pretrained(model_name).to('cuda')
